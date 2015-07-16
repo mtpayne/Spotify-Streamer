619eb3692609ce4d5398c988bfad1c2216efb966
Maurice Payne <mtpayne@users.noreply.github.com>
2015-07-16 10:11:05 -0700

    Removed menu code/files since not used.

--- app/src/main/java/com/mpayne/android/spotifystreamer/DetailActivity.java
+++ app/src/main/java/com/mpayne/android/spotifystreamer/DetailActivity.java
@@ -15,10 +15,8 @@
  */
 package com.mpayne.android.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -29,26 +27,4 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
